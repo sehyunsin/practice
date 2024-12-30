4cb4c6819f0925b35ea39f0d85c7f210853ce60e
shyun sin <sehyunsin@naver.com>
2024-12-30 10:26:10 +0900

    Create Next.js와 Chart.js로 데이터 시각화 구현.js

---
+++ Next.js와 Chart.js로 데이터 시각화 구현.js
@@ -0,0 +1,43 @@
+//npm install chart.js react-chartjs-2
+
+//pages/charts.js
+import React from 'react';
+import { Bar, Line, Pie } from 'react-chartjs-2';
+
+export default function Charts() {
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: 'Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h1>Data Visualization with Chart.js</h1>
+      <Bar data={data} />
+      <Line data={data} />
+      <Pie data={data} />
+    </div>
+  );
+}
