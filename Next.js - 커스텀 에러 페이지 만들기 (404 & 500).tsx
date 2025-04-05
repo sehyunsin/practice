cc3c7e60c2d7ecb0da239d8f8957a40dc43c121a
shyun sin <sehyunsin@naver.com>
2025-04-05 13:42:31 +0900

    Create Next.js - 커스텀 에러 페이지 만들기 (404 & 500).tsx

---
+++ Next.js - 커스텀 에러 페이지 만들기 (404 & 500).tsx
@@ -0,0 +1,10 @@
+// pages/404.tsx
+export default function Custom404() {
+    return <h1>404 - 페이지를 찾을 수 없습니다 🙈</h1>;
+  }
+  
+  // pages/500.tsx
+  export default function Custom500() {
+    return <h1>500 - 서버 오류가 발생했어요 😵</h1>;
+  }
+  
